4a6abc3adc06c347d6831fa174c87edf320baa11
xH4wks <ger.defalco@studenti.unina.it>
2021-11-07 13:39:37 +0100

    questo è il primo commit, il primo di una lunga serie, si spera

---
+++ ProgettoCodice/app/src/main/java/com/example/natour2021/Login.java
@@ -0,0 +1,46 @@
+package com.example.natour2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+    //apre l'activity per la registrazione
+    public void signuppremuto(View view)
+    {
+        Intent intent = new Intent(Login.this, Sign_Up.class);
+        startActivity(intent);
+    }
+    //apre l'activity per il recupero della password
+    public void passworddimenticatapremuto (View view)
+    {
+        //TODO
+    }
+    //apre l'activity principale dell'app
+    public void signinpremuto (View view)
+    {
+        //TODO
+    }
+    //apre esegue l'accesso con google
+    public void googleaccesspremuto (View view)
+    {
+        //TODO
+    }
+    //apre esegue l'accesso con facebook
+    public void facebookaccespremuto (View view)
+    {
+        //TODO
+    }
+
+
+}
