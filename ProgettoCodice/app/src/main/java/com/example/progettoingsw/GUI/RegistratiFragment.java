e5cea7d635df3663256d8830a7b09846e9a28e16
xH4wks <ger.defalco@studenti.unina.it>
2021-12-03 16:56:38 +0100

    nomsg

--- ProgettoCodice/app/src/main/java/com/example/progettoingsw/GUI/RegistratiFragment.java
+++ ProgettoCodice/app/src/main/java/com/example/progettoingsw/GUI/RegistratiFragment.java
@@ -11,19 +11,12 @@ import android.view.ViewGroup;
 import com.example.progettoingsw.R;
 
 
-public class LoginFragment extends Fragment {
-
-
-
-    public LoginFragment() {
-        // Required empty public constructor
-    }
+public class RegistratiFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_registrati, container, false);
     }
-
 }
--- ProgettoCodice/app/src/main/java/com/example/progettoingsw/MainActivity.java
+++ ProgettoCodice/app/src/main/java/com/example/progettoingsw/MainActivity.java
@@ -1,15 +1,61 @@
 package com.example.progettoingsw;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.example.progettoingsw.GUI.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button accedi,registrati;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        accedi = findViewById(R.id.labelBottoneAccedi);
+
+        accedi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                replaceFragment(new LoginFragment());
+
+
+            }
+        });
+
+        registrati = findViewById(R.id.buttonRegistrati);
+
+        registrati.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                replaceFragment(new RegistratiFragment());
+
+            }
+        });
+
+
+        //fine on create
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager frman = getSupportFragmentManager();
+        FragmentTransaction frtr = frman.beginTransaction();
+        frtr.replace(R.id.fragmentcontainer,fragment);
+        frtr.commit();
+
     }
 
 }
