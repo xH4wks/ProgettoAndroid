e0bac36b81cf4d6fe456510838b0bf290d5e3784
xH4wks <ger.defalco@studenti.unina.it>
2021-12-06 09:28:26 +0100

    aggiunta della Home.java, google_maps_api.xml fragment della home e fragment_menu_laterale.xml per il menù laterale

---
+++ ProgettoCodice/app/src/main/java/com/example/progettoingsw/GUI/home/Home.java
@@ -0,0 +1,17 @@
+package com.example.progettoingsw.GUI.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.progettoingsw.R;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
