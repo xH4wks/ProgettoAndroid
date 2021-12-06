0274218a1d0ef29c05de70c1ae1dfa54e45ffe35
xH4wks <ger.defalco@studenti.unina.it>
2021-12-06 19:22:17 +0100

    modifiche per la sync su android studio

--- ProgettoCodice/app/src/main/java/com/example/progettoingsw/MainActivity.java
+++ ProgettoCodice/app/src/main/java/com/example/progettoingsw/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 closeFragment(new Fragment());
+
                 registrati.setVisibility(0);
                 accedi.setVisibility(0);
             }
