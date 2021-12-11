11fe04f905c59814f2056572e81785800808be32
xH4wks <ger.defalco@studenti.unina.it>
2021-12-11 13:16:03 +0100

    risolto bug che non permetteva l'apertura della Home

--- ProgettoCodice/app/src/main/java/com/example/progettoingsw/GUI/LoginFragment.java
+++ ProgettoCodice/app/src/main/java/com/example/progettoingsw/GUI/LoginFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +13,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import com.example.progettoingsw.LogicCenter;
 import com.example.progettoingsw.GUI.home.Home;
+import com.example.progettoingsw.MainActivity;
 import com.example.progettoingsw.R;
 
 
 public class LoginFragment extends Fragment {
 
     EditText email,password;
+    MainActivity m;
 
 
     public LoginFragment() {
@@ -29,14 +33,14 @@ public class LoginFragment extends Fragment {
         View view= inflater.inflate(R.layout.fragment_login, container, false);
         //component
         Button accesso = (Button) view.findViewById(R.id.buttonaccedi);
-        email = (EditText) view.findViewById(R.id.textEmail);
+        email = (EditText) view.findViewById(R.id.textEmaillogin);
         password = (EditText) view.findViewById(R.id.textPassword);
         accesso.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 LogicCenter l = new LogicCenter();
-                
+
                 if (l.apriHome(email.getText(),password.getText())){
                     Intent home = new Intent(getActivity(),Home.class);
                     startActivity(home);
